50548c696d0a9a0b97a3b6935577396c4b496a9d
gabycode <86427446+gabycode@users.noreply.github.com>
2023-02-20 20:07:10 -0400

    style title

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,15 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App">hello world</div>;
+  return (
+    <div className="App">
+      <div className="sidebar">
+        <h1 className="title">
+          <span className="red-dev">Dev</span>challenges.io
+        </h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
