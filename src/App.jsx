69edada737b4889d833b78d19d20ee3f7db47bef
gabycode <86427446+gabycode@users.noreply.github.com>
2023-02-22 19:55:16 -0400

    add: button component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Buttons from "./components/Buttons";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
+      <Buttons />
     </div>
   );
 }
