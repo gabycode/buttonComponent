ec487d36548d6e76f02cdd490c09cb128e560a2b
gabycode <86427446+gabycode@users.noreply.github.com>
2023-02-23 23:53:18 -0400

    first button row html

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -4,6 +4,16 @@ export default function Buttons() {
   return (
     <div className="main">
       <h1 className="title">Buttons</h1>
+      <div className="button-row">
+        <div className="btn-container">
+          <pre className="button-subtitle">&lt;Button/&gt;</pre>
+          <button>Default</button>
+        </div>
+        <div className="btn-container">
+          <pre className="button-subtitle">&:hover, &:focus</pre>
+          <button>Default</button>
+        </div>
+      </div>
     </div>
   );
 }
