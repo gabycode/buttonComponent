50f8c4bc487e1e046da2c584157b42793ccd397e
gabycode <86427446+gabycode@users.noreply.github.com>
2023-02-23 23:56:11 -0400

    second and third button row html

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -4,12 +4,37 @@ export default function Buttons() {
   return (
     <div className="main">
       <h1 className="title">Buttons</h1>
+
       <div className="button-row">
-        <div className="btn-container">
-          <pre className="button-subtitle">&lt;Button/&gt;</pre>
+        <div className="btns-container">
+          <pre className="button-subtitle">&lt;Button /&gt;</pre>
           <button>Default</button>
         </div>
-        <div className="btn-container">
+        <div className="btns-container">
+          <pre className="button-subtitle">&:hover, &:focus</pre>
+          <button>Default</button>
+        </div>
+      </div>
+
+      <div className="button-row">
+        <div className="btns-container">
+          <pre className="button-subtitle">
+            &lt;Button variant="outline" /&gt;
+          </pre>
+          <button>Default</button>
+        </div>
+        <div className="btns-container">
+          <pre className="button-subtitle">&:hover, &:focus</pre>
+          <button>Default</button>
+        </div>
+      </div>
+
+      <div className="button-row">
+        <div className="btns-container">
+          <pre className="button-subtitle">&lt;Button variant="text" /&gt;</pre>
+          <button>Default</button>
+        </div>
+        <div className="btns-container">
           <pre className="button-subtitle">&:hover, &:focus</pre>
           <button>Default</button>
         </div>
