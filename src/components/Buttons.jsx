b814e7a318c4d9197ca0c91633443f8f13d40694
gabycode <86427446+gabycode@users.noreply.github.com>
2023-03-02 21:36:47 -0400

    fix: div class names

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import "../App.css";
 
 export default function Buttons() {
+  const [disable, setDisable] = useState(false);
+
   return (
     <div className="main">
       <h1 className="title">Buttons</h1>
@@ -39,6 +42,30 @@ export default function Buttons() {
         <div className="btns-container">
           <pre className="button-subtitle">&:hover, &:focus</pre>
           <button className="text">Default</button>
+        </div>
+      </div>
+
+      <div className="button-row">
+        <div className="btns-container">
+          <pre className="button-title disableShadow">
+            &lt;Button disableShadow /&gt;
+          </pre>
+          <button disableShadow className="disableShadow">
+            Default
+          </button>
+        </div>
+      </div>
+
+      <div className="button-row">
+        <div className="btns-container">
+          <pre className="button-title disabled">
+            &lt;Button {disable ? "disabled" : ""} /&gt;
+          </pre>
+          <button
+            onClick={() => setDisable(!disable)}
+            className={disable ? "disable" : "not-disable"}>
+            {disable ? "Disabled" : "Default"}
+          </button>
         </div>
       </div>
     </div>
